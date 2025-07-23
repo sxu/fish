2998fc8f78994ec0de055498332b0819a54f1030
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-23 07:06:49 -0700

    More.

--- config.fish
+++ config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     starship init fish | source
+    set -Ux STARSHIP_CONFIG ~/dotfiles/starship.toml
+end
+
+if status is-login
+	fish_add_path -g /home/sxu/.local/bin
+	fish_add_path -g /home/sxu/.cargo/bin
+	fish_add_path -g /home/sxu/.fzf/bin
 end
