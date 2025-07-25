2de9621f4ca5c58198fa45c943157778cdec18b4
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-25 06:16:51 -0700

    More.

--- config.fish
+++ config.fish
@@ -1,12 +1,14 @@
-if status is-interactive
-    starship init fish | source
-    set -Ux STARSHIP_CONFIG ~/dotfiles/starship.toml
-    zoxide init --cmd cd fish | source
-    set -Ux _ZO_MAXAGE 1000
-end
-
 if status is-login
     fish_add_path -g /home/sxu/.local/bin
     fish_add_path -g /home/sxu/.cargo/bin
     fish_add_path -g /home/sxu/.fzf/bin
+end
+
+if status is-interactive
+    set -gx FZF_DEFAULT_COMMAND 'fd --type f'
+    set -gx STARSHIP_CONFIG ~/dotfiles/starship.toml
+    set -gx _ZO_MAXAGE 1000
+
+    zoxide init --cmd cd fish | source
+    starship init fish | source
 end
